211811fecd0a765b31f92b11ae1b0a95fa55a929
Oloap008 <paolohenry008@gmail.com>
2024-05-09 10:52:39 +0800

    added global search

---
+++ lib/actions/general.action.ts
@@ -0,0 +1,83 @@
+"use server";
+
+import Question from "@/database/question.model";
+import Answer from "@/database/answer.model";
+import Tag from "@/database/tag.model";
+import User from "@/database/user.model";
+import { connectToDatabase } from "../mongoose";
+import { SearchParams } from "./shared.types";
+
+const SEARCHABLETYPES = ["question", "answer", "user", "tag"];
+
+export async function globalSearch(params: SearchParams) {
+  try {
+    connectToDatabase();
+
+    const { query, type } = params;
+    const regexQuery = { $regex: query, $options: "i" };
+
+    let results = [];
+
+    const modelsAndTypes = [
+      { model: Question, searchField: "title", type: "question" },
+      { model: User, searchField: "name", type: "user" },
+      { model: Tag, searchField: "name", type: "tag" },
+      { model: Answer, searchField: "content", type: "answer" },
+    ];
+
+    const typeLower = type?.toLowerCase();
+
+    if (!typeLower || !SEARCHABLETYPES.includes(typeLower)) {
+      // Seach everything
+      for (const { model, searchField, type } of modelsAndTypes) {
+        const queryResults = await model
+          .find({ [searchField]: regexQuery })
+          .limit(2);
+
+        results.push(
+          ...queryResults.map((item) => ({
+            title:
+              type === "answer"
+                ? `Answers containing ${query}`
+                : item[searchField],
+            type,
+            id:
+              type === "user"
+                ? item.clerkId
+                : type === "answer"
+                  ? item.question
+                  : item._id,
+          }))
+        );
+      }
+    } else {
+      // Search by specified model type
+      const modelInfo = modelsAndTypes.find((item) => item.type === typeLower);
+
+      if (!modelInfo) throw new Error("Invalid search type");
+
+      const queryResults = await modelInfo.model
+        .find({ [modelInfo.searchField]: regexQuery })
+        .limit(8);
+
+      results = queryResults.map((item) => ({
+        title:
+          type === "answer"
+            ? `Answers containing ${query}`
+            : item[modelInfo.searchField],
+        type,
+        id:
+          type === "user"
+            ? item.clerkId
+            : type === "answer"
+              ? item.question
+              : item._id,
+      }));
+    }
+
+    return JSON.stringify(results);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
